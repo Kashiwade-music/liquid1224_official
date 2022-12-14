edc1755d20d1c6e4e2cecfffb5f6ce7851e471d6
kashiwade <music5d7_1415@outlook.com>
2022-12-14 22:22:12 +0900

    feat: うおおおおおおおお

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,19 +1,83 @@
 import React from "react";
 import { Component } from "react";
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.css"
-import "../styles/carousel.css"
-import CAROUSEL1 from "../images/carousel1.jpg"
+import "react-responsive-carousel/lib/styles/carousel.css";
+import "../styles/carousel.css";
+import CAROUSEL1 from "../images/carousel1.jpg";
+import { StaticImage } from "gatsby-plugin-image";
+import { useState, useEffect } from "react";
 
-export default function TopCarousel(){
-    return(
-        <Carousel className="carousel" autoPlay interval={5500} infiniteLoop transitionTime={500} showThumbs={false} showStatus={false} showIndicators={false}>
-            <div>
-                <img src={CAROUSEL1}/>
-            </div>
-            <div>
-                <img src={CAROUSEL1}/>
-            </div>
-        </Carousel>
-    )
+export default function TopCarousel() {
+  const getWindowDimensions = () => {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  };
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+  useEffect(() => {
+    const onResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+    window.addEventListener("resize", onResize);
+    return () => window.removeEventListener("resize", onResize);
+  }, []);
+
+  let height =
+    windowDimensions.width / (windowDimensions.height - 80) > 16 / 9
+      ? `${windowDimensions.height - 80}px`
+      : "auto";
+
+  return (
+    <div>
+      <Carousel
+        className="carousel"
+        autoPlay
+        interval={5500}
+        infiniteLoop
+        transitionTime={500}
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={CAROUSEL1}
+            className="carousel_img"
+            style={{
+              margin: "auto",
+              maxWidth: "100%",
+              height: height,
+            }}
+          />
+        </div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img
+            src={CAROUSEL1}
+            className="carousel_img"
+            style={{
+              margin: "auto",
+              maxWidth: "100%",
+              height: height,
+            }}
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
